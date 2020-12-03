0ff23adb1f1d1bffc40eefe434027c4c3701341f
ELENA BUTUZA <elena.butuza@stud.ubbcluj.ro>
2020-12-03 18:17:21 +0200

    Lab9

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient.Server;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -11,9 +12,14 @@ namespace Butuza_Elena_Lab8.Models
     public class Book
     {
         public int ID { get; set; }
+        [Required, StringLength(150, MinimumLength = 3)]
         [Display(Name = "Book Title")]
         public string Title { get; set; }
+        [RegularExpression(@"^[A-Z][a-z]+\s[A-Z][a-z]+$", ErrorMessage = "Numele autorului trebuie sa fie de forma 'Prenume Nume'")
+            , Required, StringLength(50,MinimumLength = 3)]
         public string Author { get; set; }
+        [Range(1, 300)]
+
         [Column(TypeName = "decimal(6, 2)")]
         public decimal Price { get; set; }
         [DataType(DataType.Date)]
